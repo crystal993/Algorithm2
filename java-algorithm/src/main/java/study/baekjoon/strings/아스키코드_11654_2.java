b9e09be37f0f681a5b72e2916b38ce291b130548
unknown <ggg7152@gmail.com>
2022-04-04 01:23:39 +0900

    아스키코드_11654 풀이1 숫자 -> 문자, 형변환(char)

---
+++ java-algorithm/src/main/java/study/baekjoon/strings/아스키코드_11654_2.java
@@ -0,0 +1,14 @@
+package study.baekjoon.strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 아스키코드_11654_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        System.out.println((char)a);
+    }
+}

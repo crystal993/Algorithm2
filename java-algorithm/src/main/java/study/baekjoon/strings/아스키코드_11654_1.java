936bea35c158e81bd7a645b6ef96878ab3a68e8e
unknown <ggg7152@gmail.com>
2022-04-04 01:28:01 +0900

    아스키코드_11654 주석 추가

--- java-algorithm/src/main/java/study/baekjoon/strings/아스키코드_11654_1.java
+++ java-algorithm/src/main/java/study/baekjoon/strings/아스키코드_11654_1.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 public class 아스키코드_11654_1 {
     public static void main(String[] args) throws IOException {
+        // 문자 -> 숫자
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         char a = br.readLine().charAt(0);
--- java-algorithm/src/main/java/study/baekjoon/strings/아스키코드_11654_2.java
+++ java-algorithm/src/main/java/study/baekjoon/strings/아스키코드_11654_2.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 public class 아스키코드_11654_2 {
     public static void main(String[] args) throws IOException {
+        // 숫자 -> 문자
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int a = Integer.parseInt(br.readLine());

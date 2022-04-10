6cb8e8e62b9ad6e008510f0de70549cd9e66e4b6
unknown <ggg7152@gmail.com>
2022-04-10 18:33:10 +0900

    javaStudy - 이진,8진,10진수,16진수

---
+++ java-algorithm/src/main/java/study/Inflearn/String/문장속단어_p3.java
@@ -0,0 +1,7 @@
+package study.Inflearn.String;
+
+public class 문장속단어_p3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java-algorithm/src/main/java/study/javaStudy/BinaryTest.java
@@ -0,0 +1,15 @@
+package study.javaStudy;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        int num = 10;
+        int bNum = 0B1010;	// 2진수로 10을 표현
+        int oNum = 012;	//8진수로 10을 표현
+        int xNum = 0XA;	//16진수로 10을 표현
+
+        System.out.println(num);
+        System.out.println(bNum);
+        System.out.println(oNum);
+        System.out.println(xNum);	}
+}
+

502e9b732072289c2db1221433feed10678c8e9d
unknown <ggg7152@gmail.com>
2022-04-11 18:33:29 +0900

    javaStudy - 기본자료형 8개

---
+++ java-algorithm/src/main/java/study/javaStudy/VariableTest.java
@@ -0,0 +1,17 @@
+package study.javaStudy;
+
+public class VariableTest {
+    public static void main(String[] args) {
+
+        // byte의 범위 -128 ~ 127
+        byte num = -128;
+        System.out.println("num = " + num); //성공
+
+//        byte num2 = 128;
+//        System.out.println("num2 = " + num2); //오류
+
+//        int num3 = 12345678908; // 범위 초과
+          long lnum = 12345678908l; // 성공
+
+    }
+}

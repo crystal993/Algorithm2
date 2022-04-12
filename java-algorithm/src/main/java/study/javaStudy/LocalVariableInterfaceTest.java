efef4a8e29312a1571d0ad0e2f3a7c39b7ac31a6
unknown <ggg7152@gmail.com>
2022-04-12 21:54:55 +0900

    javaStudy - 지역변수 자료형 없이 사용하기(자바 10이상만)

---
+++ java-algorithm/src/main/java/study/javaStudy/LocalVariableInterfaceTest.java
@@ -0,0 +1,14 @@
+package study.javaStudy;
+
+public class LocalVariableInterfaceTest {
+    public static void main(String[] args) {
+        var i = 10;	//타입 int
+        var j = 10.0;	//타입 double
+        var str = "hello"; //타입 String
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(str);
+        str = "test";		//str = 3;
+    }
+}

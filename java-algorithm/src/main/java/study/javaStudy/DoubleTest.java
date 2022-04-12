a603a838d362ee2eb4f2a7b2c0ddedc52470f1d2
unknown <ggg7152@gmail.com>
2022-04-12 20:59:09 +0900

    javaStudy - Double, float 자료형

---
+++ java-algorithm/src/main/java/study/javaStudy/DoubleTest.java
@@ -0,0 +1,12 @@
+package study.javaStudy;
+
+public class DoubleTest {
+    public static void main(String[] args) {
+        double dnum = 3.14;
+//        float fnum = 3.14; //오류
+        float fnum = 3.14f;
+
+        System.out.println("dnum = " + dnum);
+        System.out.println("fnum = " + fnum);
+    }
+}

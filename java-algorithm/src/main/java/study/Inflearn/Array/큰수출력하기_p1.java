84bf58117f66a44b9fabe978c030cee0591fc59c
unknown <ggg7152@gmail.com>
2022-04-14 18:03:47 +0900

    array-큰수출력하기_p2

---
+++ java-algorithm/src/main/java/study/Inflearn/Array/큰수출력하기_p1.java
@@ -0,0 +1,22 @@
+package study.Inflearn.Array;
+
+import java.util.Scanner;
+
+public class 큰수출력하기_p1 {
+
+    public static void main(String[] args) {
+        큰수출력하기_p1 T = new 큰수출력하기_p1();
+        Scanner sc = new Scanner(System.in);
+
+        String answer = "";
+        int n = Integer.parseInt(sc.next());
+        int pre = -1;
+
+        for (int i = 0; i < n; i++) {
+            int af = Integer.parseInt(sc.next());
+            if(af > pre) answer += (String.valueOf(af) + " ");
+            pre = af;
+        }
+        System.out.println(answer);
+    }
+}

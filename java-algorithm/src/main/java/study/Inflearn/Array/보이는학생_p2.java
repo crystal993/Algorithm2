7e57d4eea64df5516f0ef45876297b7a98831f3c
unknown <ggg7152@gmail.com>
2022-04-15 16:05:16 +0900

    array보이는학생_p2

---
+++ java-algorithm/src/main/java/study/Inflearn/Array/보이는학생_p2.java
@@ -0,0 +1,22 @@
+package study.Inflearn.Array;
+
+import java.util.Scanner;
+
+public class 보이는학생_p2 { //9분
+
+    // 값이 큰 경우가 몇번인지 세는 문제이다.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = -1; //최댓값 담을 변수
+        int cnt = 0; // 값이 큰 경우를 세는 변수
+        for (int i = 0; i < n; i++) {
+            int height = sc.nextInt();
+            if(height > max){
+                max = height;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}

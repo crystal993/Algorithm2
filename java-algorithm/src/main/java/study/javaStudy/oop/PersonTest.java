bf97510e5d389c3bba6db11b7f503b2fec1a201e
unknown <ggg7152@gmail.com>
2022-04-18 23:49:01 +0900

    javaStudy - 객체지향입문 복습 예제1

---
+++ java-algorithm/src/main/java/study/javaStudy/oop/PersonTest.java
@@ -0,0 +1,10 @@
+package study.javaStudy.oop;
+
+public class PersonTest {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Tomas",37,180,78,"남성");
+        person1.showPersonInfo();
+
+    }
+}

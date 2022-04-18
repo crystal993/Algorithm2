946dad4a524ad6212cb59dee7ee358536b13b9da
unknown <ggg7152@gmail.com>
2022-04-18 17:25:14 +0900

    array - 뒤집은소수_p7

---
+++ java-algorithm/src/main/java/study/Inflearn/Array2/점수계산_p7.java
@@ -0,0 +1,24 @@
+package study.Inflearn.Array2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 점수계산_p7 { //8분
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       
+        int n = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
+
+        int num = 0, cnt=0, sum =0;
+       
+        for (int i = 0; i < n; i++) {
+            num = Integer.parseInt(str[i]);
+            if(num==1) cnt++; //1이면 1씩증가
+            else cnt = 0; //0이면 0
+            sum += cnt; //증가된 수만큼 더함
+        }
+        System.out.println(sum);
+    }
+}

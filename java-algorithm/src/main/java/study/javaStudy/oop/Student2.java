a43ff5812bc1b0473f4cfc34ee36fcb1126b6162
unknown <ggg7152@gmail.com>
2022-04-18 23:24:02 +0900

    javaStudy - 생성자와 클래스

---
+++ java-algorithm/src/main/java/study/javaStudy/oop/Student2.java
@@ -0,0 +1,20 @@
+package study.javaStudy.oop;
+
+public class Student2 {
+    public int studentNumber;
+    public String studentName;
+    public int grade;
+
+    public Student2(){}
+
+    public Student2(int studentNumber, String studentName, int grade) {
+        this.studentNumber = studentNumber;
+        this.studentName = studentName;
+        this.grade = grade;
+    }
+    
+    public String showStudentInfo(){
+        return studentName + " 학생의 학번은 " + studentNumber + "이고, " + grade + "학년 입니다." ;
+    }
+
+}

4a13e38f5fdbb5a97a00f6fd29f0c9a6cb574f02
unknown <ggg7152@gmail.com>
2022-04-20 16:54:07 +0900

    array - 격자판최대합_p9_풀이

---
+++ java-algorithm/src/main/java/study/CodingTestBasic/Math/Max.java
@@ -0,0 +1,12 @@
+package study.CodingTestBasic.Math;
+
+public class Max {
+    public static void main(String[] args) {
+        int num1 = 54;
+        int num2 = 35;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("최댓값 : "+Math.max(num1,num2));
+    }
+}
--- java-algorithm/src/main/java/study/Inflearn/array3/P9격자판최대합_풀이.java
+++ java-algorithm/src/main/java/study/Inflearn/array3/P9격자판최대합_풀이.java
@@ -2,7 +2,7 @@ package study.Inflearn.array3;
 
 import java.util.Scanner;
 
-public class P9격자판최대합 { //30분 이상
+public class P9격자판최대합_풀이 { //30분 이상
     public int solution(int n, int[][] arr) {
         int answer = 0, sum1 = 0, sum2 = 0;
         for (int i = 0; i < n; i++) {
@@ -12,14 +12,8 @@ public class P9격자판최대합 { //30분 이상
                 sum2 += arr[j][i]; //2. 각 행의 합(가로)
             }
             //최댓값 구하기
-            if( answer < sum1 || answer < sum2){
-                if( sum2 < sum1){
-                    answer = sum1;
-                } else {
-                    answer = sum2;
-                }
-            }
-
+            answer = Math.max(answer,sum1);
+            answer = Math.max(answer,sum2);
         }
 
         sum1 = 0; sum2 = 0;
@@ -27,19 +21,14 @@ public class P9격자판최대합 { //30분 이상
             sum1 += arr[i][i]; //3. 오른쪽 아래로 내려가는 대각선 힙
             sum2 += arr[i][n-i-1]; //4. 왼쪽 아래로 내려가는 대각선 합
         }
-        //최댓값 구하기
-        if( answer < sum1 || answer < sum2){
-            if( sum2 < sum1){
-                answer = sum1;
-            } else {
-                answer = sum2;
-            }
-        }
+        // 최댓값 구하기
+        answer = Math.max(answer,sum1);
+        answer = Math.max(answer,sum2);
         return answer;
     }
 
     public static void main(String[] args) {
-        P9격자판최대합 T = new P9격자판최대합();
+        P9격자판최대합_풀이 T = new P9격자판최대합_풀이();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[][] = new int[n][n];

8fc64d674242d106a664f9433b7df64139e34401
unknown <ggg7152@gmail.com>
2022-04-23 23:43:28 +0900

    add ArrayListTest

---
+++ java-algorithm/src/main/java/study/javaStudy/oop/ch23/ArrayListTest.java
@@ -0,0 +1,22 @@
+package study.javaStudy.oop.ch23;
+
+// 외부 패키지의 클래스 Book을 import
+import study.javaStudy.oop.ch21.Book;
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+
+        ArrayList<Book> library = new ArrayList<Book>();
+
+        library.add(new Book("태백산맥1","조정래"));
+        library.add(new Book("태백산맥2","조정래"));
+        library.add(new Book("태백산맥3","조정래"));
+        library.add(new Book("태백산맥4","조정래"));
+        library.add(new Book("태백산맥5","조정래"));
+
+        for (int i = 0; i < library.size(); i++) {
+            library.get(i).showInfo(); //객체 배열 인덱스에 해당하는 정보 출력
+        }
+    }
+}
